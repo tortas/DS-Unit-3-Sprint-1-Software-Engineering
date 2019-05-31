b736a6f17e5fcb1618da2385e774a9596c7fe714
tortas <lseeback@yahoo.com>
2019-05-31 09:58:32 -0700

    add tests

---
+++ acme_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import unittest
+from acme import Product
+from acme_report import generate_products, ADJECTIVES, NOUNS
+
+
+class AcmeProductTests(unittest.TestCase):
+    """Making sure Acme products are the tops!"""
+    def test_default_product_price(self):
+        """Test default product price being 10."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.price, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
